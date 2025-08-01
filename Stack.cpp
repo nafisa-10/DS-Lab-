30964d3a80fa7a692ec959f86ace1653af22362e
nafisa-10 <nafisaahmedneha84@gmail.com>
2025-08-01 21:31:42 +0600

    Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,84 +1,94 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-const int p = 5;
-int stack[p];
+#define n 5
+int stack[n];
 int top = -1;
-
-void push() {
-    int val;
-    cout << "Enter value: ";
-    cin >> val;
-
-    if (top == p - 1) {
-        cout << "Overflow" << endl;
+void push()
+{
+     int k;
+     cout << "Enter data: ";
+     cin >> k;
+     if(top == n-1)
+     {
+        cout << "overflow" << endl;
         return;
-    }
-
-    top++;
-    stack[top] = val;
-    cout << val << " pushed" << endl;
+     }
+     else{
+           top++;
+           stack[top] = k;
+           cout << k << " pushed" << endl;
+     }
 }
-
-void pop() {
-    if (top == -1) {
-        cout << "Underflow!" << endl;
-        return;
+void pop()
+{
+    int v;
+    if(top == -1)
+    {
+        cout << "underflow" << endl;
     }
-
-    cout << "Popped: " << stack[top] << endl;
-    top--;
+    else{
+        v = stack[top];
+        cout << v << " poped" << endl;
+        top--;
+    }
 }
-
-void display() {
+void display() 
+{
     if (top == -1) {
         cout << "Stack is empty" << endl;
         return;
     }
-
     cout << "Stack elements: ";
     for (int i = top; i >= 0; i--) {
         cout << stack[i] << " ";
     }
     cout << endl;
 }
-
-void peek() {
-    if (top == -1) {
-        cout << "Stack is empty" << endl;
-    } else {
-        cout << "Top value: " << stack[top] << endl;
+void peek()
+{
+    if(top == -1)
+    {
+        cout << "empty stack" << endl;
+    }
+    else{
+        cout << "peek value:" << stack[top] << endl;
     }
 }
-
 void isFull() {
-    if (top == p - 1) cout << "Stack is full." << endl;
+    if (top == n - 1) cout << "Stack is full" << endl;
     else cout << "Stack is not full" << endl;
 }
-
 void isEmpty() {
-    if (top == -1) cout << "Stack is empty." << endl;
+    if (top == -1) cout << "Stack is empty" << endl;
     else cout << "Stack is not empty" << endl;
 }
-
-int main() {
-    int op;
-    cout << "Enter op number: ";
-    cin >> op;
-
-    while (op--) {
-        string c;
-        cout << "\nChoose one: push | pop | display | peek | isfull | isempty\n";
-        cout << "Enter c: ";
-        cin >> c;
-
-        if (c == "push") push();
-        else if (c == "pop") pop();
-        else if (c == "display") display();
-        else if (c == "peek") peek();
-        else if (c == "isfull") isFull();
-        else if (c == "isempty") isEmpty();
-        else cout << "Invalid command" << endl;
+int main()
+{
+    int choice;
+    while(choice--)
+    {
+    cout << "\n1.push |2.pop |3.display |4.peek |5.isfull |6.isempty |7.exit " << endl;
+    cout << "Enter choice: ";
+    cin >> choice;
+    switch (choice)
+       {
+        case 1: push();
+             break;
+        case 2: pop();
+             break;
+        case 3: display();
+             break;
+        case 4: peek();
+             break; 
+        case 5: isFull();
+             break;
+        case 6: isEmpty();
+             break;
+        case 7: cout << "exit" << endl;return 0;
+        default:  
+        cout << "Invalid choice" << endl;                                     
+ 
+       }
     }
-
-    return 0;
+  return 0;
 }
