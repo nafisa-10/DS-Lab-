d6ce8fbd369b1ecc9e648dc91e10a60e4520c210
nafisa-10 <nafisaahmedneha84@gmail.com>
2025-08-03 20:13:30 +0600

    CircularQueue.cpp

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -1,96 +1,79 @@
 #include<iostream>
 using namespace std;
 #define n 5
-int arr[n];
-int front = -1,rear = -1;
-void enqueue (int val)
-{
-    if((rear+1) % n == front)
-    {
-       cout << "Overflow" << val << endl;
-    }
-    else if(front == -1 && rear == -1)
-    {
-        front = rear = 0;
-    }
-    else
-    {
-        rear = (rear +1)%n;
-    }
-    arr[rear] = val;
-    cout << val << " enqueued" << endl;
-}
-void dequeue() 
-{
-    if (front == -1 && rear == -1) {
-        cout << "Underflow" << endl;
+int queue[n];
+int front = -1;
+int rear = -1;
+void enqueue() {
+    int k;
+    cout << "Enter data: ";
+    cin >> k;
+
+    if ((rear + 1) % n == front) {
+        cout << "Overflow" << endl;
         return;
     }
-    cout << arr[front] << " dequeued" << endl;
-    if (front == rear) 
-    {
-        front = rear = -1;
-    } 
-    else 
-    {
+    else if (rear == -1 && front == -1) {
+        rear = front = 0;
+        queue[rear] = k;
+    }
+    else {
+        rear = (rear + 1) % n;
+        queue[rear] = k;
+    }
+    cout << k << " Enqueued" << endl;
+}
+void dequeue() {
+    if (rear == -1 && front == -1) {
+        cout << "Underflow" << endl;
+    }
+    else if (rear == front) {
+        cout << queue[front] << " Dequeued" << endl;
+        rear = front = -1;
+    }
+    else {
+        cout << queue[front] << " Dequeued" << endl;
         front = (front + 1) % n;
     }
 }
-void peek() 
-{
-    if (front == -1 && rear == -1) 
-    {
-        cout << "Empty" << endl;
-    } 
-    else 
-    {
-        cout << "Front element: " << arr[front] << endl;
+void peek() {
+    if (rear == -1 && front == -1) {
+        cout << "Queue is empty" << endl;
+    }
+    else {
+        cout << "Peek value: " << queue[front] << endl;
     }
 }
-void display() 
-{
-    int i = front;
-    if (front == -1 && rear == -1) {
-        cout << "Empty" << endl;
-        return;
+void display() {
+    if (rear == -1 && front == -1) {
+        cout << "Queue is empty" << endl;
     }
-    cout << "Queue: ";
-    while (true) 
-    {
-        cout << arr[i] << " ";
-        if (i == rear)
-            break;
-        i = (i + 1) % n;
+    else {
+        cout << "Queue elements: ";
+        int i = front;
+        while (true) {
+            cout << queue[i] << " ";
+            if (i == rear)
+                break;
+            i = (i + 1) % n;
+        }
+        cout << endl;
     }
-    cout << endl;
 }
-int main() 
-{
-    int s,val;
-    while (true) 
-    {
-        cout << "\nFunction Name :" << endl;
-        cout << "1. Enqueue\n2. Dequeue\n3. Peek\n4. Display" << endl;
-        cout << "Enter your s (1-4): ";
-        cin >> s;
+int main() {
+    int choice;
+    while (true) {
+        cout << "\n1.enqueue |2.dequeue |3.peek |4.display |5.exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
 
-        switch (s) {
-            case 1:
-                cout << "Enter value to enqueue: ";
-                cin >> val;
-                enqueue(val);
-                break;
-            case 2:
-                dequeue();
-                break;
-            case 3:
-                peek();
-                break;
-            case 4:
-                display();
-                break;
-            default:
-                cout << "Invalid" << endl;
+        switch (choice) {
+            case 1: enqueue(); break;
+            case 2: dequeue(); break;
+            case 3: peek(); break;
+            case 4: display(); break;
+            case 5: cout << "exit" << endl; return 0;
+            default: cout << "Invalid choice" << endl;
         }
     }
     return 0;
